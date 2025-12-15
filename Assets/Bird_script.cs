a63efa5cac52f1668490046809c2b408f04a4bcf
BigLoogz <lucas.a.hill@gmx.co.uk>
2025-12-15 19:16:05 +0000

    added reset button
    the player can now reset the birds positon to fire again and also the bird will now fire in the aimed direction

--- Assets/Bird_script.cs
+++ Assets/Bird_script.cs
@@ -1,11 +1,15 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class Bird_script : MonoBehaviour
 {
     private bool fired = false;
     private Rigidbody2D body;
+    Vector2 objPos = new Vector2(-5f,-1f);
+
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -14,25 +18,40 @@ public class Bird_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (!fired)
+        {
+            
+            float angle = Mathf.Atan2(mousePos.y - objPos.y, mousePos.x - objPos.x) * Mathf.Rad2Deg;
+            body.rotation = angle;
+        }
+
+
         bool key = Input.GetKeyDown(KeyCode.Space);
         if (key == true && fired == false)
         {
-            fire();
+            fire(mousePos);
             body.gravityScale = 1;
             fired = true;
         }
         key = false;
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 objPos = transform.position;
-
-        float angle = Mathf.Atan2(mousePos.y - objPos.y, mousePos.x - objPos.x) * Mathf.Rad2Deg;
-        body.rotation = angle;
-
-        void fire()
+        if (Input.GetKeyDown(KeyCode.R) && fired == true)
         {
-            System.Random rand = new();
-            body.AddForce(Vector2.right * rand.Next(10, 100), ForceMode2D.Impulse);
+            body.angularVelocity = 0;
+            body.linearVelocity = new Vector2(0,0);
+            body.position = objPos;
+            body.gravityScale = 0;
+            fired = false;
         }
+
+    }
+
+    void fire(Vector2 dir)
+    {
+        System.Random rand = new();
+        if (dir.x < 0) { dir.x = dir.x * -1; }
+
+        body.AddForce( new Vector2(dir.x, dir.y) * rand.Next(500,1000));
     }
 }
